c1d9009f7926a093c1ec045a072d8da8bdcdb740
krissykrissy <shinesinthesun@yahoo.com>
2019-06-17 15:44:56 -0400

    clean up before next session

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import flask
 import test
 import run
 import glob
-import os
+import os, shutil
 from flask_uploads import UploadSet, configure_uploads, DATA
 
 
@@ -28,14 +28,16 @@ def my_form():
 def my_form_post():
 
     if request.method == 'POST':
-        print request.files
-        print request.files.getlist("data[]")
+        # clean contents of uploads/ folder before each session
+        cleanUploadFolder()
+
         uploaded_files = request.files.getlist('data[]')
         for f in uploaded_files:
             csvs.save(f)
         inputpath = "uploads/"
         messagelist = run.start(inputpath)
         path = messagelist[0]
+
         if not path:
             return flask.render_template("outputmsg.html", msglist = messagelist)
         res = ""
@@ -53,6 +55,19 @@ def my_form_post():
         
     return render_template('my-form.html')
 
+def cleanUploadFolder():
+        fp = os.path.join(os.path.dirname(app.instance_path), "uploads/")
+        
+        for file in os.listdir(fp):
+            print file
+            f_del = (os.path.join(fp, file))
+            try:
+                if os.path.isfile(f_del):
+                    os.unlink(f_del)
+                elif os.path.isdir(f_del): 
+                    shutil.rmtree(f_del)
+            except Exception as e:
+                print(e)
 
 if __name__=='__main__':
     app.run(debug=True)
